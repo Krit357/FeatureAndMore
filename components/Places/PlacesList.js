5796604e287b7897ab9ec0977bc00dcef28e2a46
Chris357 <Chris@eup.co.th>
2024-02-02 16:29:12 +0700

    edit the favorite place Items

---
+++ components/Places/PlacesList.js
@@ -0,0 +1,30 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import PlaceItem from "./PlaceItem";
+
+export default function PLacesList({ places }) {
+  if (!places || !places.lenght) {
+    return (
+      <View style={styles.fallbackcontainer}>
+        <Text style={styles.fallbackText}>No places added yet</Text>
+      </View>
+    );
+  }
+  return (
+    <FlatList
+      data={places}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => <PlaceItem place={item} />}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  fallbackcontainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  fallbackText: {
+    fontSize: 16,
+  },
+});
