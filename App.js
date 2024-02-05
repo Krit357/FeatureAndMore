35795a3a876ea6c40416032996fd43e951799751
Chris357 <Chris@eup.co.th>
2024-02-05 10:36:28 +0700

    add navigationButton

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Allplaces from "./screen/Allplaces";
 import AddPlace from "./screen/AddPlace";
+import IconButton from "./components/Places/UI/IconButton";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -11,7 +12,20 @@ export default function App() {
       <StatusBar style="dark" />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="AllPlaces" component={Allplaces} />
+          <Stack.Screen
+            name="AllPlaces"
+            component={Allplaces}
+            options={({ navigation }) => ({
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon="add"
+                  size={24}
+                  color={tintColor}
+                  onPress={() => navigation.navigate("AddPlace")}
+                />
+              ),
+            })}
+          />
           <Stack.Screen name="AddPlace" component={AddPlace} />
         </Stack.Navigator>
       </NavigationContainer>
